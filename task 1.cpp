b96a1f300776135bd732a3012047dd0c52ddeee8
anton-solov@ro.ru <anton-solov@ro.ru>
2023-11-22 08:27:55 +0400

    Fibbonaci n-number by recursive function wiith (0,1)

--- task 1.cpp
+++ task 1.cpp
@@ -1,14 +1,14 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int fib(int n)
 {
-	if (n == 0) return 0;
-	if (n == 1 || n == 2) return 1;
-
-	return fib(n - 1) + fib(n - 2);
+	if (n <= 1)
+		return n-1;
+	return abs((fib(n - 1) + fib(n - 2)));
 }
 int main()
 {
-	int n; cin >> n;
+	int n;
+	cin >> n;
 	cout << fib(n);
 }
